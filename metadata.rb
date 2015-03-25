35f6d632d35f8065fb598bc0c350b08340fe2304
Maciek Ruckgaber <maciekrb@gmail.com>
2015-03-25 13:56:38 -0500

    Bare bones

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             's3fs_proxy'
+maintainer       'Maciek Ruckgaber'
+maintainer_email 'maciekrb@gmail.com'
+license          'All rights reserved'
+description      'Installs/Configures Drupal s3fs proxy requests for CSS/JSS'
+long_description 'Installs/Configures s3fs_proxy'
+version          '0.1.0'
---
+++ recipes/default.rb
@@ -0,0 +1,46 @@
+# vim: set ft=ruby
+# Cookbook Name:: s3fs_proxy
+# Recipe:: default
+#
+# Copyright (C) 2015 YOUR_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+include_recipe 'apache2::mod_proxy'
+include_recipe 'apache2::mod_proxy_http'
+
+node[:deploy].each do |app_name, deploy|
+
+  app_domain = deploy[:domains].first
+  if deploy[:application_type] != 'php' and node[:appcfg][app_domain][:s3fs].nil?
+    Chef::Log.debug("Skipping s3fs_proxy configuration, app #{app_domain} does not seem to use s3fs")
+    next
+  end
+
+
+  domain_conf = node[:appcfg][app_domain]
+  if !domain_conf.nil? and !domain_conf[:s3fs].nil?
+    if domain_conf[:s3fs][:cdn_domain_name] and !domain_conf[:s3fs][:cdn_domain_name].nil?
+      s3fs_res_name = domain_conf[:s3fs][:cdn_domain_name]
+    else
+      s3fs_res_name = "#{domain_conf[:s3fs][:s3_bucket_name]}.s3.amazonaws.com"
+    end
+  end
+
+  directory "#{node[:apache][:dir]}/sites-available/#{app_name}.conf.d" do
+    owner 'root'
+    group 'root'
+    mode '0755'
+    action :create
+  end
+
+  template "#{node[:apache][:dir]}/sites-available/#{app_name}.conf.d/local_s3fs_proxy.conf" do
+    source 's3fs_proxy.conf'
+    mode '0644'
+    variables :s3fs_res_name => s3fs_res_name
+    notifies :reload, 'service[apache2]', :immediately
+    only_if {s3fs_res_name}
+  end
+    
+end
